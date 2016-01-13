af8d760213faa9ecd4a30d70f4a48e5dea86913d
brooke hurford and Matthew Newell <brookebhurford@gmail.com>
2016-01-13 10:43:23 -0800

    Add initial files, and conditional branching showing a celbrity date based on age and type

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+  $("form#celebrityDate").submit(function(event) {
+    var age = parseInt($("input#age").val());
+    var type = $("select#type").val();
+
+    if (age) {
+      if (type === 'musician' && age >=50) {
+        // you get older guy rod stewart
+        var oneVariable = "Rod Stewart";
+      }
+      else if (type === 'musician' && age <50) {
+        //print to the screen justin timberlake
+        var oneVariable = "Justin Timberlake";
+      }
+      else if (type === 'tv' && age >=50) {
+        //print to the screen justin timberlake
+        var oneVariable = "Rob Lowe";
+      }
+      else if (type === 'tv' && age <50) {
+        //print to the screen justin timberlake
+        var oneVariable = "Jimmy Fallon";
+      }
+      else if (type === 'movie' && age >=50) {
+        //print to the screen justin timberlake
+        var oneVariable = "George Clooney";
+      }
+      else if (type === 'movie' && age <50) {
+        //print to the screen justin timberlake
+        var oneVariable = "Taylor Lautner";
+      }
+
+      $("#celebName").text(oneVariable);
+      $("#printToScreen").show();
+
+    event.preventDefault();
+}
+});
+});
